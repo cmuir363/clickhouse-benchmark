71ff1611b76cf43bd1933122ed463b35853c9a47
Calum Muir <calum.muir@aiven.io>
2024-11-06 16:13:32 +0100

    added a queries script

---
+++ setup-scripts/queries/analytical-queries.sql
@@ -0,0 +1,114 @@
+
+
+-- Number of devices per owner
+
+SELECT ownerId, count(sensorId) as numDevicesPerOwner
+FROM iot_analytics.iot_metadata
+GROUP BY ownerId, factoryId, sensorId
+
+-- Fetch one weeks worth of historical data
+
+
+
+
+-- Fetch all sensor values for each sensor type 'Temperature' within each factory
+
+SELECT 
+    factoryId,
+    sensorId,
+    value
+FROM iot_analytics.iot_measurements_raw_temp
+WHERE sensorType = 1
+
+
+-- How many values are stored per owner, per factory
+
+SELECT 
+    ownerId,
+    factoryId,
+    count(value) as numValuesPerSensor
+FROM iot_analytics.iot_measurements_raw_temp
+GROUP BY ownerId, factoryId
+
+-- 
+-- values beyond the 95th percentile for sensor type 'Temperature' for a particular owner
+
+WITH quant_90_temp_sensors AS (
+    SELECT 
+        quantile(0.9)(value) as quant90Value
+    FROM iot_analytics.iot_measurements_raw_temp
+    WHERE sensorType = 1
+),
+owners AS (
+    SELECT 
+        ownerId
+    FROM iot_analytics.iot_metadata
+    ORDER BY rand()
+    LIMIT 1
+)
+SELECT
+    ownerId,
+    factoryId,
+    count(value) as numValues
+FROM iot_analytics.iot_measurements_raw_temp
+WHERE sensorType = 1 AND value > (SELECT quant90Value FROM quant_90_temp_sensors) AND ownerId = (SELECT ownerId FROM owners)
+GROUP BY ownerId, factoryId
+
+-- 90th quantile for each sensor type per factory
+SELECT 
+    factoryId,
+    sensorType,
+    quantile(0.9)(value) as quant90Value
+FROM iot_analytics.iot_measurements_raw_temp
+GROUP BY factoryId, sensorType
+
+-- avg, 90th quantile and 99th quantile for each sensor type in factory x, y, z
+SELECT
+    factoryId,
+    avg(value) as avgValue,
+    quantile(0.9)(value) as quant90Value,
+    quantile(0.99)(value) as quant99Value
+FROM iot_analytics.iot_measurements_raw_temp
+WHERE factoryId IN ('factory_af88af30-d741-4d99-beee-0e3ac0cd1837', 'factory_008a36db-79c0-47d1-a697-6d6233875f1b', 'factory_ab377f78-56e6-47a1-9252-035034163ed3')
+GROUP BY factoryId, sensorType
+
+-- Fetch all values for 10 random sensors 
+WITH sensors AS (
+    SELECT 
+        sensorId
+    FROM iot_analytics.iot_metadata
+    ORDER BY rand()
+    LIMIT 10
+)
+SELECT 
+    sensorId,
+    value
+FROM iot_analytics.iot_measurements_raw_temp
+WHERE sensorId IN (SELECT sensorId FROM sensors)
+
+-- Fetch the number of values for each factory for a specific owner in descending order
+WITH owners As (
+    SELECT 
+        ownerId
+    FROM iot_analytics.iot_metadata
+    ORDER BY rand()
+    LIMIT 1
+)
+SELECT 
+    ownerId,
+    factoryId,
+    count(value) as numValues
+FROM iot_analytics.iot_measurements_raw_temp
+WHERE ownerId = (SELECT ownerId FROM owners)
+GROUP BY ownerId, factoryId
+ORDER BY numValues DESC
+
+-- 
+
+SELECT 
+    database,
+    table,
+    formatReadableSize(sum(data_compressed_bytes)) AS table_size
+FROM system.parts
+WHERE database = 'iot_analytics' AND table = 'iot_measurements_raw_temp'
+GROUP BY database, table;
